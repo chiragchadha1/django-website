2f55e01810a6b21702b28761d5758e6f48f80b95
Chirag Chadha <chiragkchadha@gmail.com>
2023-10-18 16:46:11 +0200

    Minor changes to urls and teams/players

--- football/urls.py
+++ football/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 	path('', views.home, name='home'), # we have to write a view called home
     path('teams/', views.teams, name='teams'),
     path('teams/manutd', views.manutd, name='manutd'),
-    path('teams/<int:teamID>', views.teamPlayers, name='team_players')
+    path('teams/<int:teamID>', views.teamPlayers, name='teamPlayers')
 ]
