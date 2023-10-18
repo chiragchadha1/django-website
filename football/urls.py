f0260df0bf34a8442f5f92af54749198b031333a
Chirag Chadha <chiragkchadha@gmail.com>
2023-10-18 15:07:07 +0200

    Updated urls and views to allow for the site to dynamically display players based on their team

--- football/urls.py
+++ football/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 app_name = 'football' # to distinguish from other possible apps
+
 urlpatterns = [
 	path('', views.home, name='home'), # we have to write a view called home
-    path('teams/', views.teams, name='teams')
+    path('teams/', views.teams, name='teams'),
+    path('teams/manutd', views.manutd, name='manutd'),
+    path('teams/<int:teamID>', views.teamPlayers, name='team_players')
 ]
--- football/views.py
+++ football/views.py
@@ -9,3 +9,15 @@ def teams(request):
     teams_list = Team.objects.all() # objects is a static attribute. it gives us all the objects/attributes associated with that team (i.e. SELECT * FROM Team)
     context = {'list_of_teams' : teams_list}
     return render(request, 'football/teams.html', context)
+
+def manutd(request):
+    team_object = Team.objects.get(name = 'Chelsea')
+    players_list = team_object.player_set.all()
+    context = {'list_of_players': players_list, 'team': team_object}
+    return render(request, 'football/showplayers.html', context)
+
+def teamPlayers(request, teamID):
+    team_object = Team.objects.get(id = teamID)
+    players_list = team_object.player_set.all()
+    context = {'list_of_players': players_list, 'team': team_object.name}
+    return render(request, 'football/showplayers.html', context)
